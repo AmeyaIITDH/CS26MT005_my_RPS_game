fe1b211180a1f55b2788e83c53de5d9af95e3c29
AmeyaIITDH <cs26mt005@iitdh.ac.in>
2026-08-22 22:27:03 +0530

    Updated the game.py with Rules for the game

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 import random
 
-print("Hello Player. Welcome to Rock Paper and Scissors Game ")
+print("Hello Player. Welcome to Rock Paper and Scissors Game\n ")
+
+print("<------------Game Rules--------------->\n")
+print("1.Player should choose either Rock,Paper or Scissor\n2.The Computer will choose his choice\n3.Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it)\n4.If its tie then the game contiues till either the player is winner or the computer")
+
+
 
 
